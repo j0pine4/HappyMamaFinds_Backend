b6104711f1ae3c6740f68522d0fda4079bcb2a1b
Jonah Pine <jpine4@hotmail.com>
2023-06-24 10:41:24 -0400

    Static files

--- root/settings.py
+++ root/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -150,5 +154,3 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 4
 }
 
-STATIC_URL = "static/"
-STATIC_ROOT = "static/"
